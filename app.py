81aa2696952ddd4baa9a8ecc2c8a0c90722d4805
Justin Rings <justin_rings@intuit.com>
2018-05-07 11:08:00 -0700

    refactor

--- app.py
+++ app.py
@@ -1,29 +1,20 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
 from resources.Vitals import Vitals
-# from resources.Me import Me
-# from resources.Connections import Connections
 from flask_cors import CORS
+
 
 bp = Blueprint('api', __name__)
 api = Api(bp)
+app = Flask(__name__)
+app.register_blueprint(bp, url_prefix='/api')
+CORS(app, resources={r"/api/*": { "origins": "*", "supports_credentials": True}})
+
 
 # Route
-# @auth.verify_Login
 api.add_resource(Vitals, '/v1/me/vitals')
 
 
-def create_app(config_filename):
-    app = Flask(__name__)
-    # app.config.from_object(config_filename)
-    # from app import bp
-    app.register_blueprint(bp, url_prefix='/api')
-
-    CORS(app, resources={r"/api/*": { "origins": "*", "supports_credentials": True}})
-
-    return app
-
-
 if __name__ == "__main__":
-    app = create_app("config")
+    # app = create_app("config")
     app.run(debug=True)
