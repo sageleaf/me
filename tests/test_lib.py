f26d6c6b5c395b4d2cf2a4891f83c7b8649a93b4
Justin Rings <justin_rings@intuit.com>
2018-05-16 16:31:01 -0700

    add generate_verification_code test

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,6 +1,12 @@
-from app.lib.utils import generate_uuid
+from app.lib.utils import generate_uuid, generate_verification_code
 
-def test_answer():
+
+def test_generate_uuid_are_unique():
     a = generate_uuid()
     b = generate_uuid()
     assert a != b
+
+
+def test_generate_verification_code_len_six():
+    vc = generate_verification_code()
+    assert len(str(vc)) == 6
