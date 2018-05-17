153cb3bbf520d3d6eec8c36780f2624cec846ea5
Justin Rings <justin_rings@intuit.com>
2018-05-17 16:06:53 -0700

    ass scout integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from scout_apm.flask import ScoutApm
+from scout_apm.flask.sqlalchemy import instrument_sqlalchemy
 
 db = SQLAlchemy()
 
@@ -10,8 +12,11 @@ def create_app(config_filename):
     CORS(app, resources={r"/api/*": { "origins": "*", "supports_credentials": True}})
     db.init_app(app)
 
+    ScoutApm(app)
+    instrument_sqlalchemy(db)
+
     from .api import api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api')
-
+    app.config['SCOUT_NAME'] = "SAGE_LEAF_ME"
 
     return app
--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,4 +1,6 @@
-from app.lib.utils import generate_uuid, generate_verification_code
+import datetime
+from app.lib.utils import generate_uuid, generate_verification_code, is_date_passed, get_future_date, get_now
+from app.lib.crypt import decrypt
 
 
 def test_generate_uuid_are_unique():
@@ -10,3 +12,17 @@ def test_generate_uuid_are_unique():
 def test_generate_verification_code_len_six():
     vc = generate_verification_code()
     assert len(str(vc)) == 6
+
+
+def test_get_future_date():
+    f = get_future_date(90)
+    n = get_now()
+    assert f > n 
+
+
+def test_is_date_passed():
+    is_passed = is_date_passed("2011-07-16T21:46:39Z")
+    assert is_passed == True
+
+
+
